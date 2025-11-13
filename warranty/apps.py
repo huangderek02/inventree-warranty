71dcebfc472fa1e3b0fc87f5d585d1f89474d09f
huangderek02 <110649463+huangderek02@users.noreply.github.com>
2025-11-13 21:37:08 +1100

    Update apps.py

--- warranty/apps.py
+++ warranty/apps.py
@@ -1,13 +1,9 @@
 from django.apps import AppConfig
 
-
 class WarrantyConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
     name = "warranty"
     verbose_name = "Warranty"
 
     def ready(self):
-        # Define the plugin class now (after apps are populated)
         from . import create_plugin_class
-
         create_plugin_class()
